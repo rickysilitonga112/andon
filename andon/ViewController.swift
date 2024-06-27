2d33a8d931e26e3d05c30d89155141c825fd54e9
rickysilitonga112 <rickysilitonga112@gmail.com>
2024-06-27 14:36:06 +0700

    Initial Commit

---
+++ andon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  andon
+//
+//  Created by Ricky Silitonga on 27/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
